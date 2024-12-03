9faf0b0ee21ef987f7437fe1006d2315069dc308
Matteo Minin <matteominin@MacBook-Pro-di-Matteo.local>
2024-12-03 23:08:56 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+import io
+import torch
+from PIL import Image
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+from torchvision.models import resnet18
+import torchvision.transforms as transforms
+
+app = Flask(__name__)
+CORS(app)
+
+# Transform the image to a tensor
+def transform_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB') 
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    image = Image.open(io.BytesIO(image_bytes))
+    return transform(image).unsqueeze(0)
+
+    
+# Load the model
+model = resnet18(weights='ResNet18_Weights.DEFAULT')
+model.fc = torch.nn.Linear(model.fc.in_features, 10)
+state_dict = torch.load('model.pth', map_location=torch.device('cpu'))
+model.load_state_dict(state_dict['model_state_dict'])
+model.eval()
+
+labels = {
+    0: "annual_crop",
+    1: "forest",
+    2: "herbaceous_vegetation",
+    3: "highway",
+    4: "industrial_building",
+    5: "pasture",
+    6: "permanent_crop",
+    7: "residential_building",
+    8: "river",
+    9: "sea_lake"
+}
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    confidence = torch.nn.functional.softmax(outputs, dim=1)[0][y_hat].item()
+    confidences = torch.nn.functional.softmax(outputs, dim=1)[0].tolist()
+    return y_hat.item(), confidence, confidences
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['image']
+        img_bytes = file.read()
+        class_id, confidence, confidences = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class': labels[class_id], 'confidence': confidence, 'confidences': confidences})
+
+if __name__ == '__main__':
+    app.run()
